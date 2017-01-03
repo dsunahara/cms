42457833aecbf21faf39e49cedeec3feb697d245
Darren S <darren.sunahara@osi.ca.gov>
2017-01-03 18:15:33 +0000

    Pages Setup

--- Gemfile
+++ Gemfile
@@ -26,6 +26,11 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass'
 gem 'haml-rails'
 gem 'puma'
+gem 'ckeditor'
+gem 'carrierwave'
+gem 'mini_magick'
+gem 'responders'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
+  resources :pages, only: [:show]
+  namespace :admin do
+    resources :pages # /admin/pages
+  end
+  
+  Page.where.not("slug", nil).all.each do |page|
+    get"/#{page.slug}", controller: "pages", action: "show", id: page.id
+  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
